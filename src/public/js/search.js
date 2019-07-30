79c8d96fc819deb962ab57b37858b43aaf72f10c
Oleg <olegboychuk099@gmail.com>
2019-07-30 17:33:06 +0300

    bug fixed

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -3,6 +3,7 @@ const result = document.querySelector('.results__map');
 const map = document.querySelector('.map');
 const sortItems = document.querySelector('.sort__items');
 const resultsList = document.querySelector('.results__list');
+const results = document.querySelector('.results__results');
 // On click
 result.addEventListener('click', () => {
   // Toggle class "is-active"
@@ -10,9 +11,19 @@ result.addEventListener('click', () => {
     resultsList.classList.remove('list--active');
     sortItems.classList.remove('list--active');
     map.classList.add('list--active');
+    results.classList.add('list--active');
+    result.classList.remove('list--active');
   } else {
     resultsList.classList.add('list--active');
     sortItems.classList.add('list--active');
     map.classList.remove('list--active');
   }
+});
+results.addEventListener('click', () => {
+  // Toggle class "is-active"
+    resultsList.classList.add('list--active');
+    sortItems.classList.add('list--active');
+    map.classList.remove('list--active');
+    result.classList.add('list--active');
+    results.classList.remove('list--active');
 });
